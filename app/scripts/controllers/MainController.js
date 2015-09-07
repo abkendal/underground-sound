a79446e92aa174ade9d8b608f4513d47f14218d6
abkendal <kendal.adam@mail.com>
2015-09-07 13:42:07 -0400

    Get soundcloud iframe src injection to work

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -1,23 +1,22 @@
-app.controller('MainController', ['$scope', '$http', function($scope, $http){
+app.controller('MainController', ['$scope', '$http', '$sce', function($scope, $http, $sce){
 
 	$scope.api_key = 'FPTUEWIJXBRDVF0EU';
 	$scope.search = '';
+	$scope.google="http://google.com";
+	$scope.song = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/207761424&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true',
 	$scope.artists = [
 		{
 			name:'Corbin Butler',
 			genre: ['rap', 'hip hop', 'indie rock', 'rap rock', 'trap music', 'underground rap'],
-			songs: [
-				'<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?soundcloud=https%3A//api.soundcloud.com/tracks/207761424&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
-				],
+			sing:'width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/207761424&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"',
+			song: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/207761424&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true',
 			soundcloud: 'https://soundcloud.com/corbin-butler',
 			desc: 'Young rapper/musician from Souther California'
 		},
 		{
 			name:'KAMAU',
 			genre: ['rap', 'hip hop', 'singer-songwriter', 'underground rap'],
-			songs: [
-				'<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?soundcloud=https%3A//api.soundcloud.com/tracks/220599163&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
-				],
+			song: 'https://w.soundcloud.com/player/?soundcloud=https%3A//api.soundcloud.com/tracks/220599163&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true',
 			soundcloud: 'https://soundcloud.com/kamauothm',
 			desc: 'Rapper/singer from Brooklyn.'
 		},
@@ -25,9 +24,7 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 		{
 			name:'CHINAH',
 			genre: ['pop', 'indie pop', 'indie rock', 'indie pop rock', 'shoegaze', 'emo', 'indie emo', 'indie folk', 'chillwave', "melancholia", "permanent wave"],
-			songs: [
-				'<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?soundcloud=https%3A//api.soundcloud.com/tracks/213453701&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
-				],
+			song: 'https://w.soundcloud.com/player/?soundcloud=https%3A//api.soundcloud.com/tracks/213453701&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true',
 			soundcloud: 'https://soundcloud.com/chinahchinah',
 			desc: 'Pop trio from Denmark'
 		},
@@ -35,9 +32,7 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 		{
 			name:'Emily Jones',
 			genre: ['pop', 'indie pop', 'indie pop rock', 'shoegaze', 'emo', 'indie emo', 'indie folk', 'singer-songwriter', 'electronic', 'chillwave', "melancholia", "permanent wave"],
-			songs: [
-				'<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?soundcloud=https%3A//api.soundcloud.com/tracks/188161303&color=f2346d"></iframe>',
-				],
+			song: 'https://w.soundcloud.com/player/?soundcloud=https%3A//api.soundcloud.com/tracks/188161303&color=f2346d',
 			soundcloud: 'https://soundcloud.com/emilyrmj',
 			desc: 'Electro-pop vocalist from the UK'
 		},
@@ -45,9 +40,7 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 		{
 			name:'kaya',
 			genre: ['indie pop', 'indie pop rock', 'shoegaze', 'emo', 'indie emo', 'indie folk', 'singer-songwriter', 'acoustic pop', 'deep acoustic pop', "melancholia", "permanent wave"],
-			songs: [
-				'<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?soundcloud=https%3A//api.soundcloud.com/tracks/217690010&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
-				],
+			song: 'https://w.soundcloud.com/player/?soundcloud=https%3A//api.soundcloud.com/tracks/217690010&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true',
 			soundcloud: 'https://soundcloud.com/kaya2281',
 			desc: 'Singer/songwriter from NYC'
 		},
@@ -55,9 +48,7 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 		{
 			name:'Her',
 			genre: ['indie rock', 'rock', 'blues', 'chill groove', 'dance rock', 'dance-punk'],
-			songs: [
-				'<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?soundcloud=https%3A//api.soundcloud.com/tracks/222138957&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
-				],
+			song: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/215176984&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true',
 			soundcloud: 'https://soundcloud.com/thebandher',
 			desc: 'Dance-punk duo from France'
 		},
@@ -65,9 +56,7 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 		{
 			name:'P2THEGOLDMA$K',
 			genre: ['future ambient', 'experimental', 'electro', 'trap music', 'deep psytrance', 'rap', 'chillwave', 'dark wave', 'emo', 'rap', 'hip hop'],
-			songs: [
-				'https://soundcloud.com/p2ski/s-e-n-p-a-i-2-ep-mix-by-burger-pmg',
-				],
+			song: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/215176984&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true',
 			soundcloud: 'https://soundcloud.com/p2ski',
 			desc: 'Emo thugg rapper from the internet.'
 		},
@@ -75,16 +64,14 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 		{
 			name:'Kweku Collins',
 			genre: ['singer-songwriter', 'pop', 'pop rap', 'alternative hip hop', 'indie rock'],
-			songs: [
-				'<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/215673535&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
-				],
+			song: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/215673535&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true',
 			soundcloud: 'https://soundcloud.com/kwekucollins',
 			desc: 'Singer/rapper from Illinois.'
 		},
 		// {
 		// 	name:'',
 		// 	genre: [],
-		// 	songs: [
+		// 	song: [
 				
 		// 		],
 		// 	soundcloud: '',
@@ -120,25 +107,34 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 
 	// Find recommendation artists that have a genre that matches at least one genre from the API response
 	$scope.artistMatch = function(response){
-
+		$scope.matches = [];
 		// Each artist in the database
 		for (var i = 0; i < $scope.artists.length; i++){
 			var match = 0;
 			// Each genre for the database artist
 			for (var j = 0; j < $scope.artists[i].genre.length; j++){
 				for (var k = 0; k < response.length; k++){
+					// Look for matching genres
 					if (response[k] === $scope.artists[i].genre[j]) {
-						console.log($scope.artists[i]);
 						match = 1;
 					};
 				};
+				// If there is a matching genre, move onto the next artist
 				if (match === 1) {
 					match = 0;
+					$scope.matches.push($scope.artists[i]);
 					break;
 				}
 			};
 		};
-		
+		$scope.artistRand($scope.matches);
+	};
+
+	$scope.artistRand = function(matches){
+		var selection = Math.round(Math.random() * (matches.length - 1));
+		$scope.suggestion = matches[selection];
+		$scope.suggestionUrl = $sce.trustAsResourceUrl($scope.suggestion.song);
+		console.log($scope.suggestion);
 	};
 
 
---
+++ app/scripts/directives/suggestion.js
@@ -0,0 +1,9 @@
+app.directive('suggestion', function() { 
+  return { 
+    // restrict: 'E', 
+    // scope: { 
+    // }, 
+    templateUrl: 'scripts/directives/suggestion.html',
+
+  }; 
+});
